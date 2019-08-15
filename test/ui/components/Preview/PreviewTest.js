c57a755217b85af893c8549ead71f925555a994f
BJR Matos <bjrmatos@gmail.com>
2019-08-15 15:42:26 -0500

    fix tests

--- test/ui/components/Preview/PreviewTest.js
+++ test/ui/components/Preview/PreviewTest.js
@@ -6,6 +6,6 @@ import { shallow } from 'enzyme'
 // don't find out such kind of component tests very much useful, but contributions welcome
 describe('<Preview />', () => {
   it('calls componentDidMount', () => {
-    shallow(<Preview />).contains(<div id='overlay' style={{display: 'none'}}></div>).should.be.ok()
+    shallow(<Preview />).contains(<div style={{ display: 'none' }} />).should.be.ok()
   })
 })
